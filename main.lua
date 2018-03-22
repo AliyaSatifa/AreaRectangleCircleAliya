a7c03deb39df955ac7916085d4037e01e2a4ef4b
Aliya Satifa <36854189+AliyaSatifa@users.noreply.github.com>
2018-03-22 10:32:58 -0400

    fixed errors

--- main.lua
+++ main.lua
@@ -38,9 +38,9 @@ myRectangle:setStrokeColor(0, 1, 0)
 areaOfRectangle = widthOfRectangle * heightOfRectangle
 
 -- write the area on the screen. Take into consideration the size of the font when positioning it on the screen
-areaText = displays.newText("The area of this rectangle with a width of \n" ..
+areaText = display.newText("The area of this rectangle with a width of \n" ..
 	 widthOfRectangle .. " and a height of " .. heightOfRectangle .. " is " ..
-	 areaOfRectangle .. " pixels^2 .", 0, 0, Ariel, textsize)
+	 areaOfRectangle .. " pixels^2 .", 0, 0, Arial, textsize)
 
 -- anchor the text and set its (x,y) position
 areaText.anchorX = 0
